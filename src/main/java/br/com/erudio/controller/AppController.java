561ddc8bce1b7eae02ad1dff5008c9be3623968b
leandrocgsi <leandrocgsi@gmail.com>
2017-03-06 20:27:40 -0300

    Quick refactory

--- src/main/java/br/com/erudio/controller/AppController.java
+++ src/main/java/br/com/erudio/controller/AppController.java
@@ -8,15 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
 
-	@RequestMapping("/")
-	String home(ModelMap modal) {
-		modal.addAttribute("title","CRUD Example");
-		return "index";
-	}
+    @RequestMapping("/")
+    String home(ModelMap modal) {
+        modal.addAttribute("title","CRUD Example");
+        return "index";
+    }
 
-	@RequestMapping("/partials/{page}")
-	String partialHandler(@PathVariable("page") final String page) {
-		return page;
-	}
-
+    @RequestMapping("/partials/{page}")
+    String partialHandler(@PathVariable("page") final String page) {
+        return page;
+    }
 }
