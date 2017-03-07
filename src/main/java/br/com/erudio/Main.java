b7afd107f95f5349370ee172b4feb35d3706f588
leandrocgsi <leandrocgsi@gmail.com>
2017-03-07 20:28:53 -0300

    Quick refactory

--- src/main/java/br/com/erudio/Main.java
+++ src/main/java/br/com/erudio/Main.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration @EnableAutoConfiguration @ComponentScan
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
