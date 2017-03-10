0da245771e7956b65ec00624f04155d6a79c2a0a
leandrocgsi <leandrocgsi@gmail.com>
2017-03-10 20:41:21 -0300

    Implementing course CRUD

---
+++ src/main/java/br/com/erudio/model/Course.java
@@ -0,0 +1,51 @@
+package br.com.erudio.model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="TBL_CURSO")
+public class Course implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="ID", nullable=false)
+    private Long id;
+
+    @NotEmpty
+    @Column(name="NOME", nullable=false)
+    private String name;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Course other = (Course) obj;
+        if (id == null) {
+            if (other.id != null) return false;
+        } else if (!id.equals(other.id)) return false;
+        if (name == null) {
+            if (other.name != null) return false;
+        } else if (!name.equals(other.name)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", name=" + name + "]";
+    }
+}
