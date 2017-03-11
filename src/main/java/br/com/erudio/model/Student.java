30eedecb2e43445c5f4ead76566a2e923a221571
leandrocgsi <leandrocgsi@gmail.com>
2017-03-11 20:56:43 -0300

    Implementing student CRUD

--- src/main/java/br/com/erudio/model/Student.java
+++ src/main/java/br/com/erudio/model/Student.java
@@ -73,8 +73,10 @@ public class Student implements Serializable{
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + ((course == null) ? 0 : course.hashCode());
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((registration == null) ? 0 : registration.hashCode());
         return result;
     }
 
@@ -84,17 +86,23 @@ public class Student implements Serializable{
         if (obj == null) return false;
         if (getClass() != obj.getClass()) return false;
         Student other = (Student) obj;
+        if (course == null) {
+            if (other.course != null) return false;
+        } else if (!course.equals(other.course)) return false;
         if (id == null) {
             if (other.id != null) return false;
         } else if (!id.equals(other.id)) return false;
         if (name == null) {
             if (other.name != null) return false;
         } else if (!name.equals(other.name)) return false;
+        if (registration == null) {
+            if (other.registration != null) return false;
+        } else if (!registration.equals(other.registration)) return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Course [id=" + id + ", name=" + name + "]";
+        return "Student [id=" + id + ", registration=" + registration + ", name=" + name + ", course=" + course + "]";
     }
 }
